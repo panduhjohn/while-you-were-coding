2e1ebf1fc71d0ff5abe1265dc1813bec2cda09fd
Colin Jaffe <balloonasaurus@gmail.com>
2019-07-17 13:41:02 -0400

    initial commit

---
+++ main.js
@@ -0,0 +1,61 @@
+console.log('\nChallenge #1\n');
+
+// Console out the numbers from 1-20, but only if they're divisible by 3.
+
+// Use `if` and a modulus operation.
+
+
+console.log('\nChallenge #2\n');
+
+// Same problem as #1, but no `if`s, `mods`, or `but`s (and no, that last one is
+// not a thing). If you did it right, you should run the loop 6 times with 1
+// console log per loop. That's called... a clue!
+
+
+console.log('\nChallenge #3\n');
+
+// Console out the numbers from 1-20. BACKWARDS.
+
+// (And, optionally, in heels.)
+
+// (That's a reference that only Olds make, and only if they're SUPER CULTURED.)
+
+// (No extra credit will be given for wearing heels during this challenge.)
+
+
+console.log('\nChallenge #4\n');
+
+// For the string 'Boy howdy am I good at this!', console out the letters
+// individually.
+
+
+console.log('\nChallenge #5\n');
+
+// For the string 'Am I the best?', console out the letters individually, AND
+// UPPERCASED.
+
+
+console.log('\nChallenge #6\n');
+
+// For the string 'Whoa, I am the best!', console out the letters individually,
+// but also capitalize the letters in the third word.
+
+
+console.log('\nChallenge #7\n');
+
+
+// For the string 'I am become death, destroyer of worlds.', console out the letters
+//  individually, but backwards. That is, the first console log should be
+// `!`, followed by `s`, followed by `i`, and so on.
+
+
+console.log('\nChallenge #8\n');
+
+const names = 'Colin|Mesuara|Drew|In Seop|Emma|Ben|J|Ryan|Olgy|Mike|Chris|John';
+
+// Console out the individual letters of each person in class today, which are
+// in the string above.
+
+// You'll know you're at a new person's name when you hit the pipe character.
+
+// Don't print the pipes!
